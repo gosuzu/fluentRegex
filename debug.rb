9b258952f46c941b76725087b202571d4a44fd19
go suzuki <gosuzuki@MMadminnoMacBook-puro-2.local>
2018-05-08 13:46:22 +0900

    commit

--- debug.rb
+++ debug.rb
@@ -3,10 +3,13 @@ require 'fluent/parser'
 require 'fluent/engine'
 
 #log_sample
-log = '12.345.67.89	-	-	[07/Mar/2015:06:59:59 +0900]	"GET /test/test HTTP/1.1"	200	58215	"-	->	/test/test/"	"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"	"-"	"SESSIONID=123456; Path=/"	"123456"	text/html'
+log = '127.0.0.1 	 - 	 [08/May/2018:13:39:38 +0900] 	 "GET /test.html?aaa HTTP/1.1"	304 	 0 	 "-"	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36" 	 "-" 	 "-"'
 
-#log_format
-regexp = /^(?<host>.+?)\t(?<logname>.+?)\t(?<user>.+?)\t(?<time_stamp>.+?)\t(?<request>.+?)\t(?<http_status>.+?)\t(?<content_length>.+?)\t(?<http_refere>.+?)\t(?<allow>.+?)\t(?<path_info>.+?)\t(?<user_agent>.+?)\t(?<cookie>.+?)\t(?<set_cookie>.+?)\t(?<custom>.+?)\t(?<content_type>.+?)$/
+#nginx logformat#
+# log_format  main  '$remote_addr \t $remote_user \t [$time_local] \t "$request"' \t '$status \t $body_bytes_sent \t "$http_referer"' \t '"$http_user_agent" \t "$http_x_forwarded_for" \t "$request_body"';
+
+regexp = /^(?<remote_addr>.+?)\t(?<remote_user>.+?)\t(?<time_stamp>.+?)\t(?<request>.+?)\t(?<http_status>.+?)\t(?<content_length>.+?)\t(?<http_refere>.+?)\t(?<user_agent>.+?)\t(?<forwarded>.+?)\t(?<request_body>.+?)$/
+
 
 #parser
 parser = Fluent::TextParser::RegexpParser.new(regexp)
